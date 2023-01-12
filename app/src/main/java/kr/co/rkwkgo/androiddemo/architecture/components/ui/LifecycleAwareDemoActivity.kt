e44e9a9f43e4c4cc7bd8b3f5332cca683b14eb7d
김도현 <>
2023-01-12 17:10:49 +0900

    feat : ViewModel

---
+++ app/src/main/java/kr/co/rkwkgo/androiddemo/architecture/components/ui/LifecycleAwareDemoActivity.kt
@@ -0,0 +1,31 @@
+package kr.co.rkwkgo.androiddemo.architecture.components.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.rkwkgo.androiddemo.databinding.ActivityLifecycleAwareDemoBinding
+
+/**
+ * Lifecycle-Aware Components
+ * https://developer.android.com/topic/libraries/architecture/lifecycle
+ * Activity, Fragments components 의 lifecycle 상태 변경에 반응해서 액션을 수행한다.
+ * better-organized, lighter-weight code, easier to maintain
+ */
+class LifecycleAwareDemoActivity : AppCompatActivity() {
+
+	private lateinit var binding: ActivityLifecycleAwareDemoBinding
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityLifecycleAwareDemoBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+		binding.btnViewModel.setOnClickListener {
+			goViewModelDemoActivity()
+		}
+	}
+
+	private fun goViewModelDemoActivity(){
+		val intent = Intent(this, ViewModelDemoActivity::class.java)
+		startActivity(intent)
+	}
+}
