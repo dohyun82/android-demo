d68dd926b2d5ee212d86e6ecc6d1001bfc46e2d7
김도현 <>
2023-04-10 16:34:52 +0900

    feat : 상태 호이스팅 처리

--- app/src/main/java/kr/co/rkwkgo/androiddemo/composenew/material3/ComposeMaterial3Activity.kt
+++ app/src/main/java/kr/co/rkwkgo/androiddemo/composenew/material3/ComposeMaterial3Activity.kt
@@ -3,13 +3,13 @@ package kr.co.rkwkgo.androiddemo.composenew.material3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.co.rkwkgo.androiddemo.composenew.material3.ui.theme.AndroidDemoTheme
 
 class ComposeMaterial3Activity : ComponentActivity() {
@@ -17,27 +17,116 @@ class ComposeMaterial3Activity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			AndroidDemoTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(
-					modifier = Modifier.fillMaxSize(),
-					color = MaterialTheme.colorScheme.background
-				) {
-					Greeting("Android")
-				}
+				MyApp(modifier = Modifier.fillMaxSize())
 			}
 		}
 	}
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+private fun MyApp(
+	modifier: Modifier = Modifier,
+){
+
+	var shouldShowOnboarding by remember {
+		mutableStateOf(true)
+	}
+
+	Surface(modifier = modifier) {
+		if(shouldShowOnboarding){
+			OnboardingScreen{
+				shouldShowOnboarding = false
+			}
+		}else{
+			Greetings()
+		}
+	}
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked : () -> Unit){
+	Column (
+		modifier = modifier.fillMaxSize(),
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+		Text(text = "Welcome to the Basics Codelab!")
+		Button(
+			modifier = Modifier.padding(vertical = 24.dp),
+			onClick = onContinueClicked
+		) {
+			Text(text = "Continue")
+		}
+	}
+}
+
+
+@Composable
+private fun Greetings(
+	modifier: Modifier = Modifier,
+	names: List<String> = listOf("World", "Compose")
+){
+	Column(modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+		for (name in names){
+			Greeting(name = name)
+		}
+	}
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
 	AndroidDemoTheme {
-		Greeting("Android")
+		OnboardingScreen(onContinueClicked = {})
+	}
+}
+
+@Composable
+private fun Greeting(name: String){
+	val expended = remember {
+		mutableStateOf(false)
+	}
+	val extraPadding = if (expended.value) 48.dp else 0.dp
+
+	Surface(
+		color = MaterialTheme.colorScheme.primary,
+		modifier = Modifier.padding(vertical = 4.dp)) {
+		Row(
+			modifier = Modifier.padding(24.dp)
+		){
+			Column(
+				modifier = Modifier
+					.weight(1F)
+					.padding(extraPadding)
+			) {
+				Text(text = "Hello,")
+				Text(text = name)
+			}
+			ElevatedButton(
+				onClick = {
+					expended.value = !expended.value
+				},
+			) {
+				Text(if (expended.value) "Show less" else "Show more")
+			}
+		}
+
+	}
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview(){
+	AndroidDemoTheme {
+		Greetings()
+	}
+}
+
+@Preview
+@Composable
+fun MyAppPreview(){
+	AndroidDemoTheme {
+		MyApp(Modifier.fillMaxSize())
 	}
 }
