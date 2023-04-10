c9e917871697b1bfcb7ac4c123149858ef7b7a07
김도현 <>
2023-04-10 16:41:25 +0900

    feat : LazyColumn 적용

--- app/src/main/java/kr/co/rkwkgo/androiddemo/composenew/material3/ComposeMaterial3Activity.kt
+++ app/src/main/java/kr/co/rkwkgo/androiddemo/composenew/material3/ComposeMaterial3Activity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +31,7 @@ private fun MyApp(
 	modifier: Modifier = Modifier,
 ){
 
-	var shouldShowOnboarding by remember {
+	var shouldShowOnboarding by rememberSaveable {
 		mutableStateOf(true)
 	}
 
@@ -65,10 +68,10 @@ fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked : () -> Un
 @Composable
 private fun Greetings(
 	modifier: Modifier = Modifier,
-	names: List<String> = listOf("World", "Compose")
+	names: List<String> = List(1000) {"$it"}
 ){
-	Column(modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-		for (name in names){
+	LazyColumn(modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+		items(items = names) { name ->
 			Greeting(name = name)
 		}
 	}
