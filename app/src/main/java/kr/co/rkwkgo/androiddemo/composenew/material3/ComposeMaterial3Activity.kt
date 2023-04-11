ba1808a89931fe591b4ea32926cd89081b2af98d
김도현 <>
2023-04-11 10:41:23 +0900

    feat : 테마 적용, Card 적용

--- app/src/main/java/kr/co/rkwkgo/androiddemo/composenew/material3/ComposeMaterial3Activity.kt
+++ app/src/main/java/kr/co/rkwkgo/androiddemo/composenew/material3/ComposeMaterial3Activity.kt
@@ -1,21 +1,29 @@
 package kr.co.rkwkgo.androiddemo.composenew.material3
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kr.co.rkwkgo.androiddemo.R
 import kr.co.rkwkgo.androiddemo.composenew.material3.ui.theme.AndroidDemoTheme
 
 class ComposeMaterial3Activity : ComponentActivity() {
@@ -34,7 +42,7 @@ private fun MyApp(modifier: Modifier = Modifier){
 
 	var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
-	Surface(modifier = modifier) {
+	Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) {
 		if(shouldShowOnboarding){
 			OnboardingScreen(onContinueClicked ={ shouldShowOnboarding = false } )
 		}else{
@@ -76,52 +84,81 @@ private fun Greetings(
 	}
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun OnboardingPreview(){
-	AndroidDemoTheme {
-		OnboardingScreen(onContinueClicked = {})
+private fun Greeting(name: String){
+	Card(
+		colors = CardDefaults.cardColors(
+			containerColor = MaterialTheme.colorScheme.primary
+		),
+		modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+	) {
+		CardContent(name = name)
 	}
 }
 
 @Composable
-private fun Greeting(name: String){
-
+private fun CardContent(name: String){
 	var expended by remember { mutableStateOf(false) }
 
-	val extraPadding by animateDpAsState(
-		targetValue = if (expended) 48.dp else 0.dp,
-		animationSpec = spring(
-			dampingRatio = Spring.DampingRatioMediumBouncy,
-			stiffness = Spring.StiffnessLow
-		)
-	)
-	Surface(
-		color = MaterialTheme.colorScheme.primary,
-		modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-	) {
-		Row(modifier = Modifier.padding(24.dp)){
-			Column(modifier = Modifier
-					.weight(1F)
-					.padding(bottom = extraPadding.coerceAtLeast(0.dp))
-			) {
-				Text(text = "Hello,")
-				Text(text = name)
+	Row(
+		modifier = Modifier
+			.padding(12.dp)
+			.animateContentSize(
+				animationSpec = spring(
+					dampingRatio = Spring.DampingRatioMediumBouncy,
+					stiffness = Spring.StiffnessLow
+				)
+			)
+	){
+		Column(modifier = Modifier
+			.weight(1F)
+			.padding(12.dp)
+		) {
+			Text(text = "Hello,")
+			Text(
+				text = name,
+				style = MaterialTheme.typography.headlineMedium.copy(
+					fontWeight = FontWeight.ExtraBold
+				)
+			)
+			if (expended){
+				Text(
+					text = ("Composem ipsum color sit lazy, " +
+						"padding theme elit, sed do bouncy. ").repeat(4),
+				)
 			}
-			ElevatedButton(
-				onClick = { expended = !expended }
-			) {
-				Text(if (expended) "Show less" else "Show more")
-			}
+		}
+		IconButton(onClick = { expended = !expended }) {
+			Icon(imageVector = if (expended) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+				contentDescription = if (expended) {
+					stringResource(id = R.string.show_less)
+				} else{
+					stringResource(id = R.string.show_more)
+				}
+			)
 		}
 	}
 }
 
+@Preview(
+	showBackground = true,
+	widthDp = 320,
+	uiMode = UI_MODE_NIGHT_YES,
+	name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview(){
 	AndroidDemoTheme {
 		Greetings()
+	}
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+	AndroidDemoTheme {
+		OnboardingScreen(onContinueClicked = {})
 	}
 }
 
