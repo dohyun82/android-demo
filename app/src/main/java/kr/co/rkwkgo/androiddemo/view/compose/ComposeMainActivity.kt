6dd7b9c4d84363d0376676d23c6b51d4c33c7957
김도현 <>
2023-08-09 17:23:16 +0900

    feat : 컴포즈 튜토리얼

--- app/src/main/java/kr/co/rkwkgo/androiddemo/view/compose/ComposeMainActivity.kt
+++ app/src/main/java/kr/co/rkwkgo/androiddemo/view/compose/ComposeMainActivity.kt
@@ -1,22 +1,19 @@
 package kr.co.rkwkgo.androiddemo.view.compose
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.android.material.appbar.AppBarLayout
 import kr.co.rkwkgo.androiddemo.view.compose.ui.theme.AndroidDemoTheme
 
 /**
@@ -28,7 +25,9 @@ class ComposeMainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			AndroidDemoTheme {
-				ComposeMainLayout()
+				ComposeMainLayout {
+					startActivity(Intent(this@ComposeMainActivity, ComposeTutorialActivity::class.java))
+				}
 			}
 		}
 	}
@@ -36,7 +35,7 @@ class ComposeMainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ComposeMainLayout(){
+fun ComposeMainLayout(onClick : ()-> Unit){
 //	modifier: Modifier = Modifier,
 //	topBar: @Composable () -> Unit = {},
 //	bottomBar: @Composable () -> Unit = {},
@@ -55,7 +54,7 @@ fun ComposeMainLayout(){
 		},
 	){
 		Column(modifier = Modifier.padding(it)) {
-			Button(onClick = {}) {
+			Button(onClick = onClick) {
 				Text(text = "Compose Tutorial")
 			}
 		}
@@ -65,5 +64,7 @@ fun ComposeMainLayout(){
 @Preview
 @Composable
 fun PreviewComposeMainLayout(){
-	ComposeMainLayout()
+	ComposeMainLayout {
+
+	}
 }
--- app/src/main/java/kr/co/rkwkgo/androiddemo/view/compose/ComposeTutorialActivity.kt
+++ app/src/main/java/kr/co/rkwkgo/androiddemo/view/compose/ComposeTutorialActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -19,9 +21,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -52,58 +54,55 @@ class ComposeTutorialActivity : ComponentActivity() {
 }
 
 @Composable
-fun MessageCard(msg: Message){
-	Row(modifier = Modifier.padding(all = 8.dp)){
-		Image(
-			painter = painterResource(id = R.drawable.profile_picture),
-			contentDescription = "Contact profile picture",
-			modifier = Modifier
-				.size(40.dp)
-				.clip(CircleShape)
-				.border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-		)
-		Spacer(
-			modifier = Modifier.width(8.dp)
-		)
-
-		var isExpanded by remember { mutableStateOf(false) }
-
-		val surfaceColor by animateColorAsState(targetValue =
-			if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
-		)
-
-		Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-			Text(
-				text = msg.author,
-				color = MaterialTheme.colors.secondaryVariant,
-				style = MaterialTheme.typography.subtitle2,
-			)
-			Spacer(modifier = Modifier.height(4.dp))
-
-			Surface(
-				shape = MaterialTheme.shapes.medium,
-				elevation = 1.dp,
-				color = surfaceColor,
-				modifier = Modifier.animateContentSize().padding(1.dp),
-				) {
-				Text(
-					text = msg.body,
-					modifier = Modifier.padding(all = 4.dp),
-					maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-					style = MaterialTheme.typography.body2,
-				)
-			}
+fun Conversation(messages: List<Message>){
+	LazyColumn{
+		items(messages){
+			MessageCard(msg = it)
 		}
 	}
 }
 
 @Composable
-fun Conversation(messages: List<Message>){
-	LazyColumn{
-		items(messages){ message ->
-			MessageCard(msg = message)
+fun MessageCard(msg: Message){
+
+	Row(modifier = Modifier.padding(all = 8.dp)) {
+		Image(painter = painterResource(
+			id = R.drawable.profile_picture),
+			contentDescription = "Contact profile picture",
+			modifier = Modifier
+				.size(40.dp)
+				.clip(CircleShape)
+				.border(1.5.dp, MaterialTheme.colorScheme.primary)
+			)
+		Spacer(modifier = Modifier.width(8.dp))
+
+		var isExpanded by remember { mutableStateOf(false) }
+		val surfaceColor by animateColorAsState(
+			targetValue = if(isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface
+		)
+		Column (modifier = Modifier.clickable { isExpanded = !isExpanded }){
+			Text(
+				text = msg.author,
+				color = MaterialTheme.colorScheme.secondary,
+				style = MaterialTheme.typography.titleSmall
+			)
+			Spacer(modifier = Modifier.height(4.dp))
+			Surface(
+				shape = MaterialTheme.shapes.medium,
+				shadowElevation = 1.dp,
+				color = surfaceColor,
+				modifier = Modifier.animateContentSize().padding(1.dp)
+			) {
+				Text(
+					text = msg.body,
+					modifier = Modifier.padding(all = 4.dp),
+					maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+					style = MaterialTheme.typography.bodyMedium
+				)
+			}
 		}
 	}
+
 }
 
 @Preview
@@ -114,18 +113,21 @@ fun PreviewConversation(){
 	}
 }
 
+
 @Preview(name = "Light Mode")
 @Preview(
 	uiMode = Configuration.UI_MODE_NIGHT_YES,
 	showBackground = true,
-	name = "Dark Mode",
+	name = "Dark Mode"
 )
 @Composable
 fun PreviewMessageCard(){
 	AndroidDemoTheme {
-		Surface {
+		Surface(
+			modifier = Modifier.fillMaxWidth(),
+		) {
 			MessageCard(
-				msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+				msg = Message("지은이", "블라블라블라")
 			)
 		}
 	}
