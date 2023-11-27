6c3bf9be8b6cc5f9a6c0c1dec452cd93edb16c0c
김도현 <>
2023-11-27 18:40:41 +0900

    feat : Compose 연습

--- app/src/main/java/kr/co/rkwkgo/androiddemo/view/compose/ComposeAnimationActivity.kt
+++ app/src/main/java/kr/co/rkwkgo/androiddemo/view/compose/ComposeAnimationActivity.kt
@@ -3,32 +3,25 @@ package kr.co.rkwkgo.androiddemo.view.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateColor
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateIntOffsetAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
+import androidx.compose.animation.expandHorizontally
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.togetherWith
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -36,14 +29,11 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -59,15 +49,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.layout
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.style.TextMotion
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.co.rkwkgo.androiddemo.view.compose.ui.theme.AndroidDemoTheme
 import kotlin.math.roundToInt
 
@@ -137,200 +127,17 @@ private fun AnimationBody(
 		modifier = modifier.fillMaxWidth(),
 		contentAlignment = Alignment.Center
 	){
-//		DemoAnimateAppearing()
-//		DemoBackgroundColor()
-//		DemoAnimateSize()
-//		DemoAnimatePosition()
-//		DemoAnimatePositionLayout()
-//		DemoAnimatePadding()
-//		DemoAnimateElevation()
-//		DemoAnimateTextStyle()
-//		DemoAnimateTextColor()
-//		DemoSwitchDifferentContent()
-	}
-}
-
-
-enum class UIState{
-	Loading,
-	Loaded,
-	Error
-}
-
-@Composable
-private fun DemoSwitchDifferentContent(){
-	var state by remember{
-		mutableStateOf(UIState.Loading)
-	}
-	AnimatedContent(
-		targetState = state,
-		transitionSpec = {
-			fadeIn(
-				animationSpec = tween(3000)
-			) togetherWith fadeOut(animationSpec = tween(3000))
-		},
-		modifier = Modifier.clickable(
-			interactionSource = remember { MutableInteractionSource() },
-			indication = null
-		) {
-			state = when(state){
-				UIState.Loading -> UIState.Loaded
-				UIState.Loaded -> UIState.Error
-				UIState.Error -> UIState.Loading
-			}
-		},
-		label = "Animated Content"
-	) {targetState ->
-		Box(
-			modifier = Modifier.fillMaxSize(),
-		){
-			when(targetState){
-				UIState.Loading -> {
-					Box(
-						modifier = Modifier.size(100.dp, 100.dp).background(Color.Blue).align(
-							Alignment.Center),
-						contentAlignment = Alignment.Center
-					){
-						Text("로딩중",
-							color = Color.White)
-					}
-				}
-				UIState.Loaded -> {
-					Box(
-						modifier = Modifier.size(300.dp, 300.dp).background(Color.Green).align(
-							Alignment.BottomCenter),
-						contentAlignment = Alignment.Center
-					){
-						Text("완료",
-							color = Color.White)
-					}
-				}
-				UIState.Error -> {
-					Box(
-						modifier = Modifier.size(200.dp, 200.dp).background(Color.Red).align(
-							Alignment.TopCenter),
-						contentAlignment = Alignment.Center
-					){
-						Text("에러",
-							color = Color.White)
-					}
-				}
-			}
-		}
+		DemoAnimateLayout()
 	}
 }
 
 @Composable
-private fun DemoAnimateTextColor(){
-	val infiniteTransition = rememberInfiniteTransition(
-		label = "infinite transition"
-	)
-	val animatedColor by infiniteTransition.animateColor(
-		initialValue = Color.Red,
-		targetValue = Color.Yellow,
-		animationSpec = infiniteRepeatable(tween(1000), RepeatMode.Reverse),
-		label = "color"
-	)
-	BasicText(text = "Hello Compose",
-		color = {
-			animatedColor
-		})
-}
-
-@Composable
-private fun DemoAnimateTextStyle(){
-	val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
-	val scale by infiniteTransition.animateFloat(
-		initialValue = 1f,
-		targetValue = 8f,
-		animationSpec = infiniteRepeatable(tween(1000), RepeatMode.Reverse),
-		label = "scale"
-	)
-	Box{
-		Box(modifier = Modifier.fillMaxSize()){
-			Text(
-				text = "Hello",
-				modifier = Modifier
-					.graphicsLayer {
-						scaleX = scale
-						scaleY = scale
-						transformOrigin = TransformOrigin.Center
-					}
-					.align(Alignment.Center),
-				style = LocalTextStyle.current.copy(
-					textMotion = TextMotion.Animated
-				)
-			)
-		}
-	}
-}
-
-
-@Composable
-private fun DemoAnimateElevation(){
-	val mutableInteractionSource = remember {
-		MutableInteractionSource()
-	}
-	val pressed = mutableInteractionSource.collectIsPressedAsState()
-	val elevation = animateDpAsState(
-		targetValue = if (pressed.value){
-			32.dp
-		} else {
-			8.dp
-		},
-		label = "elevation"
-	)
-	Box{
-		Box(
-			modifier = Modifier
-				.size(100.dp)
-				.align(Alignment.Center)
-				.graphicsLayer {
-					this.shadowElevation = elevation.value.toPx()
-				}
-				.clickable(
-					interactionSource = mutableInteractionSource, indication = null
-				) {}
-				.background(Color.Green)
-		)
-	}
-
-}
-
-@Composable
-private fun DemoAnimatePadding(){
+private fun DemoAnimateLayout(){
+	val isLookingAhead = false
 	var toggled by remember{
 		mutableStateOf(false)
 	}
-	val animatedPadding by animateDpAsState(
-		targetValue = if(toggled) 0.dp else 20.dp,
-		label = "padding"
-	)
-	Box(
-		modifier = Modifier
-			.aspectRatio(1f)
-			.fillMaxSize()
-			.padding(animatedPadding)
-			.background(Color.Green)
-			.clickable(
-				interactionSource = remember {
-					MutableInteractionSource()
-				},
-				indication = null
-			) { toggled = !toggled }
-	)
-}
-
-@Composable
-private fun DemoAnimatePositionLayout(){
-
-	var isLookingAhead by remember{
-		mutableStateOf(false)
-	}
-	var toggled by remember {
-		mutableStateOf(false)
-	}
-	val interactionSource = remember {
+	val interactionSource = remember{
 		MutableInteractionSource()
 	}
 	Column(
@@ -346,11 +153,12 @@ private fun DemoAnimatePositionLayout(){
 	) {
 		val offsetTarget = if (toggled){
 			IntOffset(150, 150)
-		}else {
+		}else{
 			IntOffset.Zero
 		}
 		val offset = animateIntOffsetAsState(
-			targetValue = offsetTarget, label = "offset"
+			targetValue = offsetTarget,
+			label = "offset"
 		)
 		Box(
 			modifier = Modifier
@@ -360,9 +168,9 @@ private fun DemoAnimatePositionLayout(){
 		Box(
 			modifier = Modifier
 				.layout { measurable, constraints ->
-					val offsetValue = if (isLookingAhead) offsetTarget else offset.value
+					val offsetValue = if(isLookingAhead) offsetTarget else offset.value
 					val placeable = measurable.measure(constraints)
-					layout(placeable.width + offsetValue.x, placeable.height + offsetValue.y) {
+					layout(placeable.width + offsetValue.x, placeable.height + offsetValue.y){
 						placeable.placeRelative(offsetValue)
 					}
 				}
@@ -374,15 +182,11 @@ private fun DemoAnimatePositionLayout(){
 				.size(100.dp)
 				.background(Color.Blue)
 		)
-		Button(onClick = {isLookingAhead = !isLookingAhead}) {
-			Text("isLookingAhead: $isLookingAhead")
-		}
 	}
 }
 
-
 @Composable
-private fun DemoAnimatePosition(){
+private fun DemoAnimateIntOffset(){
 	var moved by remember {
 		mutableStateOf(false)
 	}
@@ -390,13 +194,14 @@ private fun DemoAnimatePosition(){
 		100.dp.toPx().roundToInt()
 	}
 	val offset by animateIntOffsetAsState(
-		targetValue = if(moved){
+		targetValue = if (moved) {
 			IntOffset(pxToMove, pxToMove)
-		}else{
+		}else {
 			IntOffset.Zero
 		},
 		label = "offset"
 	)
+
 	Box(
 		modifier = Modifier
 			.offset {
@@ -414,7 +219,7 @@ private fun DemoAnimatePosition(){
 }
 
 @Composable
-private fun DemoAnimateSize(){
+private fun DemoAnimateContentSize(){
 	var expanded by remember {
 		mutableStateOf(false)
 	}
@@ -424,80 +229,110 @@ private fun DemoAnimateSize(){
 			.animateContentSize()
 			.height(if (expanded) 400.dp else 200.dp)
 			.fillMaxWidth()
-			.clickable(
-				interactionSource = remember {
-					MutableInteractionSource()
-				},
-				indication = null
-			) {
+			.clickable {
 				expanded = !expanded
 			}
-	)
+	){
+		Text("블라블라블라~~~\n블라블라블라~~~\n블라블라블라~~~\n블라블라블라~~~\n블라블라블라~~~\n블라블라블라~~~\n블라블라블라~~~"
+			, color = Color.White
+		)
+	}
 }
 
 @Composable
-private fun DemoAnimateBackgroundColor(){
+private fun DemoAnimateColor(){
 	var animateBackgroundColor by remember{
 		mutableStateOf(true)
 	}
 	val animatedColor by animateColorAsState(
-		targetValue = if (animateBackgroundColor) Color.Green else Color.Blue,
+		if(animateBackgroundColor) Color.Green else Color.Blue,
 		label = "color"
 	)
+
 	Column(
 		modifier = Modifier
 			.fillMaxSize()
 			.drawBehind {
 				drawRect(animatedColor)
 			}
-	) {
-		Button(onClick = { animateBackgroundColor = !animateBackgroundColor }) {
-			Text("배경색 바꾸기")
-		}
-	}
+			.clickable {
+				animateBackgroundColor = !animateBackgroundColor
+			}
+	){}
+
 }
 
 @Composable
-private fun DemoAnimateAppearing(){
-	var visible by remember{
+private fun DemoAnimateFloatAsState(){
+	var visible by remember {
 		mutableStateOf(true)
 	}
 	val animatedAlpha by animateFloatAsState(
-		targetValue = if (visible) 1.0f else 0f,
-		label = "alpha",
+		targetValue = if(visible) 1.0f else 0f,
+		label = "alpha"
 	)
-	Column(
-		horizontalAlignment = Alignment.CenterHorizontally
-	){
-		Row{
-			AnimatedVisibility(
-				visible = visible,
-				enter = expandVertically(),
-				exit = shrinkVertically()
-			) {
-				Box(
-					modifier = Modifier
-						.size(200.dp)
-						.clip(RoundedCornerShape(8.dp))
-						.background(Color.Red)
-				)
-			}
-			Box(
-				modifier = Modifier
-					.size(200.dp)
-					.graphicsLayer {
-						alpha = animatedAlpha
-					}
-					.clip(RoundedCornerShape(8.dp))
-					.background(Color.Green)
-			)
-		}
-		Button(onClick = {
-			visible = !visible
-		}) {
-			Text(
-				text = if(visible) "숨김" else "보여주기"
-			)
-		}
+	Column {
+		Box(
+			modifier = Modifier
+				.size(200.dp)
+				.padding(16.dp)
+				.graphicsLayer {
+					alpha = animatedAlpha
+				}
+				.clip(RoundedCornerShape(8.dp))
+				.background(Color.Green)
+		)
+		Box(
+			modifier = Modifier
+				.size(200.dp)
+				.padding(16.dp)
+				.clip(RoundedCornerShape(8.dp))
+				.background(Color.Blue)
+				.clickable {
+					visible = !visible
+				}
+		)
 	}
 }
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+private fun DemoAnimatedVisibility(){
+	var visible by remember{
+		mutableStateOf(true)
+	}
+	Column {
+		AnimatedVisibility(
+			visible,
+			enter = expandHorizontally(),
+			exit = shrinkHorizontally()
+		){
+			Box(
+				modifier = Modifier
+					.fillMaxWidth()
+					.height(200.dp)
+					.padding(16.dp)
+					.background(Color.Green)
+			){
+				Text("....",
+					fontSize = 90.sp,
+					modifier = Modifier.animateEnterExit(
+						enter = fadeIn(),
+						exit = fadeOut()
+						)
+					)
+			}
+		}
+		Box(
+			modifier = Modifier
+				.fillMaxWidth()
+				.height(200.dp)
+				.padding(16.dp)
+				.background(Color.Blue)
+				.clickable {
+					visible = !visible
+				}
+		)
+	}
+}
+
