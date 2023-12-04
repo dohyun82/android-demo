155c210695dfdb10d2b0d540ef9a0b243793b0ee
김도현 <>
2023-12-05 08:27:54 +0900

    feat : compose 테스트

--- app/src/main/java/kr/co/rkwkgo/androiddemo/view/compose/ComposeAnimationActivity.kt
+++ app/src/main/java/kr/co/rkwkgo/androiddemo/view/compose/ComposeAnimationActivity.kt
@@ -3,13 +3,23 @@ package kr.co.rkwkgo.androiddemo.view.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,7 +29,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,11 +37,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import kr.co.rkwkgo.androiddemo.view.compose.ui.theme.AndroidDemoTheme
 
 /**
@@ -97,30 +104,69 @@ private fun AnimationPage(){
 private fun AnimationBody(
 	modifier: Modifier = Modifier
 ){
-	Box(
+	Column(
 		modifier = modifier.fillMaxWidth(),
-		contentAlignment = Alignment.Center
 	){
-		DemoAnimateColorState()
+		DemoAnimatedContent()
 	}
 }
 
+enum class UIState{
+	Loading, Loaded, Error
+}
+
 @Composable
-private fun DemoAnimateColorState(){
-	var animateBackgroundColor by remember{
-		mutableStateOf(false)
+private fun DemoAnimatedContent(){
+	var state by remember{
+		mutableStateOf(UIState.Loading)
 	}
-	val animateColor by animateColorAsState(
-		targetValue = if(animateBackgroundColor) Color.Red else Color.Blue, label = "color"
-	)
-	Column {
-		Box(
-			modifier = Modifier.fillMaxWidth().height(100.dp).drawBehind {
-				drawRect(animateColor)
+	AnimatedContent(
+		targetState = state,
+		transitionSpec = {
+				 fadeIn(
+					 animationSpec = tween(3000)
+				 ) togetherWith fadeOut(animationSpec = tween(3000))
+		},
+		modifier = Modifier.clickable(
+			interactionSource = remember{ MutableInteractionSource() },
+			indication = null
+		) {
+		  state = when(state){
+			  UIState.Loading -> UIState.Loaded
+			  UIState.Loaded -> UIState.Error
+			  UIState.Error -> UIState.Loading
+		  }
+		},
+		label = "AnimatedContent"
+	) {targetState ->
+		when(targetState){
+			UIState.Loading -> {
+				Box(
+					modifier = Modifier.fillMaxSize()
+				){
+					Text("로딩중",
+						modifier = Modifier.align(Alignment.Center)
+					)
+				}
 			}
-		)
-		TextButton(onClick = { animateBackgroundColor = !animateBackgroundColor }) {
-			Text(text = "색상 바꾸기")
+			UIState.Loaded -> {
+				Box(
+					modifier = Modifier.fillMaxSize()
+				){
+					Text("로드 완료",
+						modifier = Modifier.align(Alignment.Center)
+					)
+				}
+			}
+			UIState.Error -> {
+				Box(
+					modifier = Modifier.fillMaxSize()
+				){
+					Text("에러",
+						modifier = Modifier.align(Alignment.Center)
+					)
+				}
+			}
 		}
 	}
 }
