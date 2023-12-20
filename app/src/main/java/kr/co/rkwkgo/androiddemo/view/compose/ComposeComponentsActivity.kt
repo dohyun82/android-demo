494b98d99107ab7bf52533bff21d5fd20097eb7f
김도현 <>
2023-12-20 13:12:57 +0900

    feat : Card compose

--- app/src/main/java/kr/co/rkwkgo/androiddemo/view/compose/ComposeComponentsActivity.kt
+++ app/src/main/java/kr/co/rkwkgo/androiddemo/view/compose/ComposeComponentsActivity.kt
@@ -3,18 +3,23 @@ package kr.co.rkwkgo.androiddemo.view.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButton
@@ -22,6 +27,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Surface
@@ -30,6 +36,8 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kr.co.rkwkgo.androiddemo.view.compose.ui.theme.AndroidDemoTheme
@@ -87,7 +95,78 @@ private fun ComponentsBody(
 	Column(
 		modifier = modifier
 	) {
-		ButtonExample()
+		OutlinedCardExample()
+	}
+}
+
+@Composable
+private fun OutlinedCardExample(){
+	OutlinedCard(
+		colors = CardDefaults.cardColors(
+			containerColor = MaterialTheme.colorScheme.surface
+		),
+		border = BorderStroke(1.dp, Color.Black),
+		modifier = Modifier
+			.size(
+				width = 240.dp,
+				height = 100.dp
+			)
+	) {
+		Text(
+			text = "Outlined",
+			modifier = Modifier
+				.padding(16.dp),
+			textAlign = TextAlign.Center
+		)
+	}
+}
+
+@Composable
+private fun ElevatedCardExample(){
+	ElevatedCard(
+		elevation = CardDefaults.cardElevation(
+			defaultElevation = 6.dp
+		),
+		modifier = Modifier
+			.size(
+				width = 240.dp,
+				height = 100.dp
+			)
+	) {
+		Text(
+			text = "Elevated",
+			modifier = Modifier
+				.padding(16.dp),
+			textAlign = TextAlign.Center,
+		)
+	}
+}
+
+@Composable
+private fun FilledCardExample(){
+	Card(
+		colors = CardDefaults.cardColors(
+			containerColor = MaterialTheme.colorScheme.surfaceVariant
+		),
+		modifier = Modifier
+			.size(
+				width = 240.dp,
+				height = 100.dp
+			)
+	){
+		Text(
+			text = "Filled",
+			modifier = Modifier
+				.padding(16.dp),
+			textAlign = TextAlign.Center
+		)
+	}
+}
+
+@Composable
+private fun CardMinimalExample(){
+	Card{
+		Text("Hello, world!")
 	}
 }
 
